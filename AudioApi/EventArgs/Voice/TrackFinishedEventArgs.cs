a3d5763be64ceb640877520b43f0bf96b4636935
史蒂夫 <steam114514@126.com>
2025-11-09 15:15:52 +0800

    Delete AudioApi directory
    改动较大 删除大部分文件

--- AudioApi/EventArgs/Voice/TrackFinishedEventArgs.cs
+++
@@ -1,17 +0,0 @@
-﻿namespace AudioApi.EventArgs.Voice
-{
-    /// <summary>
-    /// 音频播放完毕
-    /// </summary>
-    public class TrackFinishedEventArgs
-    {
-        public VoicePlayerBase VoicePlayerBase { get; }
-        public string Track { get; }
-        public bool DirectPlay { get; }
-        public TrackFinishedEventArgs(VoicePlayerBase playerBase, string track, bool directPlay)        {
-            VoicePlayerBase = playerBase;
-            Track = track;
-            DirectPlay = directPlay;
-        }
-    }
-}
